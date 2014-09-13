781874701f646f998bf82c1198b595747bb9df06
Christian Flothmann <christian.flothmann@xabbuh.de>
2014-09-13 09:15:22 +0200

    add option to configure a complex api url pattern

--- sensio/sphinx/phpcode.py
+++ sensio/sphinx/phpcode.py
@@ -13,19 +13,13 @@ from sphinx.util.nodes import split_explicit_title
 def php_namespace_role(typ, rawtext, text, lineno, inliner, options={}, content=[]):
     text = utils.unescape(text)
     env = inliner.document.settings.env
-    base_url = env.app.config.api_url
     has_explicit_title, title, namespace = split_explicit_title(text)
 
     if len(re.findall(r'[^\\]\\[^\\]', rawtext)) > 0:
         env.warn(env.docname, 'backslash not escaped in %s' % rawtext, lineno)
 
-    try:
-        full_url = base_url % namespace.replace('\\', '/') + '.html'
-    except (TypeError, ValueError):
-        env.warn(env.docname, 'unable to expand %s api_url with base '
-                 'URL %r, please make sure the base contains \'%%s\' '
-                 'exactly once' % (typ, base_url))
-        full_url = base_url + utils.escape(full_class)
+    full_url = build_url('namespace', namespace, None, None, inliner)
+
     if not has_explicit_title:
         name = namespace.lstrip('\\')
         ns = name.rfind('\\')
@@ -39,19 +33,16 @@ def php_namespace_role(typ, rawtext, text, lineno, inliner, options={}, content=
 def php_class_role(typ, rawtext, text, lineno, inliner, options={}, content=[]):
     text = utils.unescape(text)
     env = inliner.document.settings.env
-    base_url = env.app.config.api_url
     has_explicit_title, title, full_class = split_explicit_title(text)
+    backslash = full_class.rfind('\\')
+    namespace = full_class[:backslash]
+    class_name = full_class[backslash+1:]
 
     if len(re.findall(r'[^\\]\\[^\\]', rawtext)) > 0:
         env.warn(env.docname, 'backslash not escaped in %s' % rawtext, lineno)
 
-    try:
-        full_url = base_url % full_class.replace('\\', '/') + '.html'
-    except (TypeError, ValueError):
-        env.warn(env.docname, 'unable to expand %s api_url with base '
-                 'URL %r, please make sure the base contains \'%%s\' '
-                 'exactly once' % (typ, base_url))
-        full_url = base_url + utils.escape(full_class)
+    full_url = build_url('class', namespace, class_name, None, inliner)
+
     if not has_explicit_title:
         class_name = full_class.lstrip('\\')
         ns = class_name.rfind('\\')
@@ -65,23 +56,20 @@ def php_class_role(typ, rawtext, text, lineno, inliner, options={}, content=[]):
 def php_method_role(typ, rawtext, text, lineno, inliner, options={}, content=[]):
     text = utils.unescape(text)
     env = inliner.document.settings.env
-    base_url = env.app.config.api_url
     has_explicit_title, title, class_and_method = split_explicit_title(text)
 
     ns = class_and_method.rfind('::')
     full_class = class_and_method[:ns]
     method = class_and_method[ns+2:]
+    backslash = full_class.rfind('\\')
+    namespace = full_class[:backslash]
+    class_name = full_class[backslash+1:]
 
     if len(re.findall(r'[^\\]\\[^\\]', rawtext)) > 0:
         env.warn(env.docname, 'backslash not escaped in %s' % rawtext, lineno)
 
-    try:
-        full_url = base_url % full_class.replace('\\', '/') + '.html' + '#method_' + method
-    except (TypeError, ValueError):
-        env.warn(env.docname, 'unable to expand %s api_url with base '
-                 'URL %r, please make sure the base contains \'%%s\' '
-                 'exactly once' % (typ, base_url))
-        full_url = base_url + utils.escape(full_class)
+    full_url = build_url('method', namespace, class_name, method, inliner)
+
     if not has_explicit_title:
         title = method + '()'
     list = [nodes.reference(title, title, internal=False, refuri=full_url, reftitle=full_class + '::' + method + '()')]
@@ -130,9 +118,49 @@ def php_phpfunction_role(typ, rawtext, text, lineno, inliner, options={}, conten
 
 def setup(app):
     app.add_config_value('api_url', {}, 'env')
+    app.add_config_value('api_url_pattern', None, 'env')
+    app.add_config_value('namespace_separator', '/', 'env')
     app.add_role('namespace', php_namespace_role)
     app.add_role('class', php_class_role)
     app.add_role('method', php_method_role)
     app.add_role('phpclass', php_phpclass_role)
     app.add_role('phpmethod', php_phpmethod_role)
     app.add_role('phpfunction', php_phpfunction_role)
+
+def build_url(role, namespace, class_name, method, inliner):
+    env = inliner.document.settings.env
+
+    if namespace is None:
+        namespace = ''
+    if class_name is None:
+        class_name = ''
+    if method is None:
+        method = ''
+
+    if ('namespace_separator' in env.app.config):
+        namespace = namespace.replace('\\', env.app.config.namespace_separator)
+    else:
+        namespace = namespace.replace('\\', '/')
+
+    if (env.app.config.api_url_pattern is None):
+        fqcn = '%(namespace)s{class}/%(class)s{/class}{method}/%(class)s{/method}'
+        api_url_pattern = env.app.config.api_url.replace('%s', fqcn)
+        api_url_pattern += '.html{method}#method_%(method)s{/method}'
+    else:
+        api_url_pattern = env.app.config.api_url_pattern
+
+    api_url_pattern = api_url_pattern.replace('{'+role+'}', '')
+    api_url_pattern = api_url_pattern.replace('{/'+role+'}', '')
+
+    for unused_role in ('namespace', 'class', 'method'):
+        api_url_pattern = re.sub(r'{'+unused_role+'}.*?{/'+unused_role+'}', '', api_url_pattern)
+
+    try:
+        full_url = api_url_pattern % {'namespace': namespace, 'class': class_name, 'method': method}
+    except (TypeError, ValueError):
+        env.warn(env.docname, 'unable to expand %s api_url with base '
+                 'URL %r, please make sure the base contains \'%%s\' '
+                 'exactly once' % (role, api_url_pattern))
+        full_url = api_url_pattern + utils.escape(full_class)
+
+    return full_url
